7c80e9a06fdc22c546ff7c6ba0f3a0b1b0630a13
MARCUS YAP <121378260+marcus-yap@users.noreply.github.com>
2024-09-20 00:29:10 +0800

    Setup frontend using bootstrap-vue-next (#7)
    * bootstrap-vue-next setup
    
    * Setup frontend using bootstrap-vue-next
    
    * missing install

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import {createBootstrap} from 'bootstrap-vue-next'
 
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+import './assets/custom.css';
+
 const app = createApp(App)
 
+app.use(createBootstrap())
 app.use(createPinia())
 app.use(router)
 
