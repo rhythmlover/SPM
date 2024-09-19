cd0146aba3315e530c79337e9bccd79d8a8cd754
MARCUS YAP <121378260+marcus-yap@users.noreply.github.com>
2024-09-20 00:29:10 +0800

    Setup frontend using bootstrap-vue-next (#7)
    * bootstrap-vue-next setup
    
    * Setup frontend using bootstrap-vue-next
    
    * missing install

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
   ],
   resolve: {
     alias: {
