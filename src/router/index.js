8c90f2b4fa133cf447c332c56143f331170185ba
CarissaChia <133015320+CarissaChia@users.noreply.github.com>
2024-09-21 10:24:22 +0800

    Feature/staff view wfh request status (#17)
    * staff navbar, staffRequestView
    
    * readme edits
    
    ---------
    
    Co-authored-by: Nigel Koh <kohyuanqi@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import staffMySchedule from "../views/staffMySchedule.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: "/staffMySchedule",
+      name: "staffMySchedule",
+      component: staffMySchedule,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/staffTeamSchedule",
+      name: "staffTeamSchedule",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../views/staffTeamSchedule.vue"),
     },
     {
-      path: "/test",
-      name: "test",
+      path: "/staffRequestStatus",
+      name: "staffRequestStatus",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/TestView.vue"),
+      component: () => import("../views/StaffRequestStatus.vue"),
     },
   ],
 });
