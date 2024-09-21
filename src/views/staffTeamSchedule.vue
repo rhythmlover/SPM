be4701315c43f0ec53f7d3790a800f3756c77e88
Wx <26271681+JamesCrr@users.noreply.github.com>
2024-09-21 11:04:16 +0800

    Removed rebase error in staffTeamSchedule

--- src/views/staffTeamSchedule.vue
+++ src/views/staffTeamSchedule.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="about">
-<<<<<<<< HEAD:src/views/staffTeamSchedule.vue
     <h1>This is a staff view of the team's schedule</h1>
-========
     <h1>Staff page</h1>
->>>>>>>> 5bc00b8 (Backend endpoints, frontend login):src/views/StaffView.vue
   </div>
 </template>
 
