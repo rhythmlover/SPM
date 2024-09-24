8484dae5edb04bf554b1afdb01fc5d3b656e80ff
Wx <26271681+JamesCrr@users.noreply.github.com>
2024-09-24 18:46:55 +0800

    Fixed CI failing issues

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,14 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
-import viteConfig from './vite.config'
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url))
-    }
-  })
-)
+      exclude: [...configDefaults.exclude, 'e2e/**', 'backend/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+    },
+  }),
+);
