3dd06abee7a9a205fb7ac39ec2a1494d8edf95fb
Nigel Koh <kohyuanqi@gmail.com>
2024-09-27 16:57:28 +0800

    google sheets auto update when ci pipeline runs

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -3,20 +3,28 @@ import { closeConnection } from '../mysqlConnection';
 import mysql from 'mysql2/promise';
 import request from 'supertest';
 import app from '../server.js';
+import { updateSheet } from '../../updateGoogleSheet';
 
 describe('Server Tests', () => {
   jest.setTimeout(60000);
-
-  beforeAll(async () => {});
 
   afterAll(async () => {
     await closeConnection();
   });
 
   it('Test Ping', async () => {
-    const response = await request(app).get('/'); // Replace with your endpoint
+    const testId = 'TC-001';
 
-    expect(response.status).toBe(200);
-    expect(response.body).toEqual({ message: 'Tesing Ping Success!' });
+    try {
+      const response = await request(app).get('/');
+
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual({ message: 'Testing Ping Success!' });
+
+      await updateSheet(testId, 'Passed');
+    } catch (error) {
+      await updateSheet(testId, 'Failed');
+      throw error;
+    }
   });
 });
