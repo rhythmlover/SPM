37adfc66afcd6c076ff64958a35c0a6e5745b277
Wx <26271681+JamesCrr@users.noreply.github.com>
2024-09-28 22:27:06 +0800

    Filtering by day or week

--- src/views/staff/StaffMySchedule.vue
+++ src/views/staff/StaffMySchedule.vue
@@ -6,12 +6,19 @@ import IBiUpArrow from '~icons/bi/caret-up-fill';
 
 const userStore = useUserStore();
 // Get the current date and viewing date
-const currentDate = ref(new Date());
+const todayDate = ref(new Date());
 const viewingDate = ref(new Date());
+const viewingMonth = computed(() => viewingDate.value.toLocaleString('default', { month: 'long' }));
 const monthDiff = ref(0);
-const currentMonth = computed(() => viewingDate.value.toLocaleString('default', { month: 'long' }));
-const datesInMonth = ref({});
+const datesInViewingMonth = ref({}); // Store days in the viewing month
+const weeksInViewingMonth = ref({}); // Store weeks in the viewing month
 const API_ROUTE = inject('API_ROUTE');
+// Filtering by day or month
+const dayView = ref(true);
+const dayWeekFilterDropdownSelectOptions = [
+  { value: true, text: 'Day' },
+  { value: false, text: 'Week' },
+];
 
 // Handle changing month click
 const disablePreviousMonthButton = computed(() => monthDiff.value - 1 < -2);
@@ -28,7 +35,7 @@ const handleChangeMonthClick = async (movement) => {
   newDate.setMonth(newDate.getMonth() + movement);
   // Assign new date
   viewingDate.value = newDate;
-  await getDatesInMonth();
+  await getDatesInViewingMonth();
 };
 
 // Get WFH Requests
@@ -52,7 +59,7 @@ const getWFHRequests = async () => {
   }
 };
 // Calculate the days in the current month with formatting and requests
-const getDatesInMonth = async () => {
+const getDatesInViewingMonth = async () => {
   const year = viewingDate.value.getFullYear();
   const month = viewingDate.value.getMonth(); // 0-based month
   const days = new Date(year, month + 1, 0).getDate(); // Get last day of the month
@@ -72,34 +79,52 @@ const getDatesInMonth = async () => {
     'December',
   ];
 
-  // Create an array of all days in the month with formatting
-  let newMap = {};
+  // Create array of all days & weeks in viewing month
+  let newWeeks = [];
+  let currentWeek = [];
+  let newDaysMap = {};
   for (let i = 0; i < days; i++) {
+    // New date
     const date = new Date(year, month, i + 1);
     const dayName = date.toLocaleString('default', { weekday: 'long' });
     const formattedDate = `${monthNames[month]} ${i + 1}, ${year} (${dayName})`;
-    // New entry
-    newMap[date.toLocaleDateString('en-CA')] = { dateObj: date, formattedDate, requests: [] };
+    // Add day to days map
+    let newDayObj = { dateObj: date, formattedDate, requests: [] };
+    newDaysMap[date.toLocaleDateString('en-CA')] = newDayObj;
+
+    // Add day to current week
+    currentWeek.push(newDayObj);
+    // If day is Sunday (date.getDay() == 6), push the current week to newWeeks and start a new week
+    if (date.getDay() == 6) {
+      newWeeks.push(currentWeek);
+      currentWeek = [];
+    }
   }
-  datesInMonth.value = newMap;
+  // Leftover days in currentWeek, push as final week
+  if (currentWeek.length > 0) {
+    newWeeks.push(currentWeek);
+  }
+  // Update refs
+  datesInViewingMonth.value = newDaysMap;
+  weeksInViewingMonth.value = newWeeks;
 
   // Get all requests in month
   let requests = await getWFHRequests();
   for (let requestObj of requests) {
     let newRequestObj = { ...requestObj };
     delete newRequestObj['Dates'];
-    // Add dates of requests to the datesInMonth object with the same date
+    // Add dates of requests to the datesInViewingMonth object with the same date
     for (let requestDate of requestObj['Dates']) {
-      if (!(requestDate['WFH_Date'] in datesInMonth.value)) {
+      if (!(requestDate['WFH_Date'] in datesInViewingMonth.value)) {
         continue;
       }
-      // Add request's date
-      datesInMonth.value[requestDate['WFH_Date']]['requests'].push({
+      // Add request's date to the obj
+      datesInViewingMonth.value[requestDate['WFH_Date']]['requests'].push({
         ...newRequestObj,
         Date: requestDate,
       });
       // console.log('requestDate["WFH_Date"]: ', requestDate['WFH_Date']);
-      // console.log(datesInMonth.value[requestDate['WFH_Date']]);
+      // console.log(datesInViewingMonth.value[requestDate['WFH_Date']]);
     }
   }
 };
@@ -123,31 +148,20 @@ const getDateCollapseIconRotated = (visible) => {
   };
 };
 
-// Use onMounted to initialize currentDate once
 onMounted(async () => {
-  currentDate.value = new Date();
+  // Use onMounted to initialize dates once
+  todayDate.value = new Date();
   viewingDate.value = new Date();
-  await getDatesInMonth();
+  await getDatesInViewingMonth();
 });
 </script>
 
 <template>
-  <BContainer>
+  <BContainer :style="{ marginTop: '100px' }">
     <BRow>
       <BCol>
-        <h1>Staff Schedule</h1>
+        <h1>My Schedule</h1>
       </BCol>
-    </BRow>
-    <BRow>
-      <BCol cols="3">
-        <BDropdown text="Dropdown">
-          <BDropdownItem @click="console.log('FIRST')">First Action</BDropdownItem>
-          <BDropdownItem @click="console.log('SECOND')">Second Action</BDropdownItem>
-          <BDropdownItem @click="console.log('THIRD')">Third Action</BDropdownItem>
-          <BDropdownItem disabled>Disabled action</BDropdownItem>
-        </BDropdown>
-      </BCol>
-      <BCol cols="3"> </BCol>
     </BRow>
     <BRow>
       <BCol>
@@ -156,7 +170,7 @@ onMounted(async () => {
         </BButton>
       </BCol>
       <BCol class="text-center">
-        <h3>{{ currentMonth }}</h3>
+        <h3>{{ viewingMonth }}</h3>
       </BCol>
       <BCol class="d-flex justify-content-end"
         ><BButton
@@ -168,17 +182,21 @@ onMounted(async () => {
         ></BCol
       >
     </BRow>
+    <!-- Filters -->
+    <BRow class="my-2">
+      <!-- Day / Week Filter -->
+      <BCol class="col-4 col-md-2">
+        <BFormSelect v-model="dayView" :options="dayWeekFilterDropdownSelectOptions" />
+      </BCol>
+    </BRow>
     <BRow>
-      <!-- Render all dates in month -->
-      <BCol>
-        <div v-for="(dateValueObject, dateKeyStr) in datesInMonth" :key="dateKeyStr" class="my-3">
-          <!-- <div class="d-block" role="button" v-b-toggle="dateKeyStr">
-            <div class="d-flex justify-content-between">
-              {{ dateValueObject['formattedDate'] }}
-              <IBiUpArrow style="">
-            </div>
-          </div> -->
-
+      <!-- Render all dates in month in day view -->
+      <BCol v-if="dayView == true">
+        <div
+          v-for="(dateValueObject, dateKeyStr) in datesInViewingMonth"
+          :key="dateKeyStr"
+          class="my-3"
+        >
           <!-- Element to collapse -->
           <BCollapse :id="dateKeyStr">
             <template #header="{ visible, toggle, id }">
@@ -197,23 +215,90 @@ onMounted(async () => {
             </template>
 
             <BCard>
-              <BListGroup>
-                <BListGroupItem
-                  v-for="requestObj in dateValueObject['requests']"
-                  :key="requestObj['Request_ID']"
-                >
-                  <div>
-                    {{
-                      `${requestObj['Staff']['Staff_FName']} ${requestObj['Staff']['Staff_LName']}`
-                    }}
-                    {{ requestObj['Staff']['Position'] }}
-                    {{ requestObj['Date']['WFH_Time'] }}
-                    <BBadge :variant="getStatusPillColor(requestObj['Status'])" pill>{{
-                      requestObj['Status']
-                    }}</BBadge>
-                  </div>
-                </BListGroupItem>
-              </BListGroup>
+              <BTableSimple hover small caption-top responsive>
+                <BThead head-variant="dark">
+                  <BTr>
+                    <BTh>Name</BTh>
+                    <BTh>Position</BTh>
+                    <BTh>WFH Time</BTh>
+                    <BTh>Status</BTh>
+                  </BTr>
+                </BThead>
+                <BTbody>
+                  <BTr
+                    v-for="requestObj in dateValueObject['requests']"
+                    :key="requestObj['Request_ID']"
+                  >
+                    <BTd>
+                      {{
+                        `${requestObj['Staff']['Staff_FName']} ${requestObj['Staff']['Staff_LName']}`
+                      }}
+                    </BTd>
+                    <BTd> {{ requestObj['Staff']['Position'] }}</BTd>
+                    <BTd> {{ requestObj['Date']['WFH_Time'] }}</BTd>
+                    <BTd>
+                      <BBadge :variant="getStatusPillColor(requestObj['Status'])" pill>
+                        {{ requestObj['Status'] }}
+                      </BBadge>
+                    </BTd>
+                  </BTr>
+                </BTbody>
+              </BTableSimple>
+            </BCard>
+          </BCollapse>
+        </div>
+      </BCol>
+
+      <!-- Render all dates in month in week view -->
+      <BCol v-else>
+        {{ console.log('weeksInViewingMonth:', weeksInViewingMonth) }}
+        <div v-for="weekList in weeksInViewingMonth" :key="weekList[0].formattedDate" class="my-3">
+          <!-- Element to collapse -->
+          <BCollapse :id="weekList[0].formattedDate">
+            <template #header="{ visible, toggle, id }">
+              <div
+                class="d-block"
+                role="button"
+                :aria-expanded="visible"
+                :aria-controls="id"
+                @click="toggle"
+              >
+                <div class="d-flex justify-content-between border-bottom border-2 fw-bold">
+                  {{
+                    weekList[0].formattedDate + ' - ' + weekList[weekList.length - 1].formattedDate
+                  }}
+                  <IBiUpArrow :style="getDateCollapseIconRotated(visible)" />
+                </div>
+              </div>
+            </template>
+
+            <BCard>
+              <BTableSimple hover small caption-top responsive>
+                <BThead head-variant="dark">
+                  <BTr>
+                    <BTh>Name</BTh>
+                    <BTh>Position</BTh>
+                    <BTh>WFH Time</BTh>
+                    <BTh>Status</BTh>
+                  </BTr>
+                </BThead>
+                <BTbody v-for="dayObj in weekList" :key="dayObj['formattedDate']">
+                  <BTr v-for="requestObj in dayObj['requests']" :key="requestObj['Request_ID']">
+                    <BTd>
+                      {{
+                        `${requestObj['Staff']['Staff_FName']} ${requestObj['Staff']['Staff_LName']}`
+                      }}
+                    </BTd>
+                    <BTd> {{ requestObj['Staff']['Position'] }}</BTd>
+                    <BTd> {{ requestObj['Date']['WFH_Time'] }}</BTd>
+                    <BTd>
+                      <BBadge :variant="getStatusPillColor(requestObj['Status'])" pill>
+                        {{ requestObj['Status'] }}
+                      </BBadge>
+                    </BTd>
+                  </BTr>
+                </BTbody>
+              </BTableSimple>
             </BCard>
           </BCollapse>
         </div>
