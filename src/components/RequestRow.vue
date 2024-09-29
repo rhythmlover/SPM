9a003eda131b6057e9bfee2092e9d56212acd95f
Nigel Koh <kohyuanqi@gmail.com>
2024-09-29 19:54:48 +0800

    refactored the entire DSPendingRequests page to utilize vue components

---
+++ src/components/RequestRow.vue
@@ -0,0 +1,54 @@
+<template>
+    <tr>
+        <td class="col-2">{{ request.Staff_FName }} {{ request.Staff_LName }}</td>
+        <td class="col-4">{{ request.Reason }}</td>
+        <td class="col-2">{{ request.WFH_Date }}</td>
+        <td class="col-2">{{ formatRequestDate(request.Request_Date) }}</td>
+        <td class="col-2" v-if="status !== 'rejected'">
+        <StatusButton v-if="status === 'pending'" 
+            @click="updateStatus('Approved')"
+            label="Accept" btnClass="btn-success"
+        />
+        <StatusButton v-if="status === 'pending'" 
+            @click="updateStatus('Rejected')"
+            label="Reject" btnClass="btn-danger"
+        />
+        <StatusButton v-if="status === 'accepted'" 
+            @click="updateStatus('Withdrawn')"
+            label="Withdraw" btnClass="btn-outline-success"
+        />
+        </td>
+        <td class="col-2" v-if="status === 'rejected'">{{ request.Comments }}</td>
+    </tr>
+</template>
+
+<script>
+import StatusButton from './StatusButton.vue';
+
+export default {
+  name: 'RequestRow',
+  props: {
+    request: Object,
+    status: String,
+  },
+  components: { StatusButton },
+  methods: {
+    formatRequestDate(date) {
+      return new Date(date).toLocaleDateString() + ' ' + new Date(date).toLocaleTimeString();
+    },
+    updateStatus(newStatus) {
+      this.$emit('updateRequestStatus', this.request.Request_ID, newStatus);
+    }
+  }
+};
+</script>
+
+<style scoped>
+td {
+  padding: 10px 25px;
+  text-align: left;
+  vertical-align: middle;
+  font-weight: normal;
+  font-size: 14px;
+}
+</style>
