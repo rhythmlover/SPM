2a4b60c94b194dd6b96e9c7611cb043a032d5935
Helliad <44154838+Helliad@users.noreply.github.com>
2024-09-30 16:43:24 +0800

    Working Staff Team Schedule

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import employee from './routes/employee.js';
 import wfh_request from './routes/wfh_request.js';
+import teamlist from './routes/teamlist.js';
 import express from 'express';
 import cors from 'cors';
 
@@ -13,6 +14,7 @@ app.use(express.json());
 // Routes
 app.use('/employee', employee);
 app.use('/wfh_request', wfh_request);
+app.use('/teamlist', teamlist);
 app.get('/', (req, res) => {
   res.json({ message: 'Testing Ping Success!' });
 });
