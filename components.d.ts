af4226174a705ce119e87d4995885dac2dc0f3e9
CarissaChia <133015320+CarissaChia@users.noreply.github.com>
2024-10-17 23:37:20 +0800

    fixed UI for staff page to match manager page

--- components.d.ts
+++ components.d.ts
@@ -20,6 +20,8 @@ declare module 'vue' {
     BDropdownForm: typeof import('bootstrap-vue-next/components/BDropdown')['BDropdownForm']
     BDropdownItem: typeof import('bootstrap-vue-next/components/BDropdown')['BDropdownItem']
     BFormCheckbox: typeof import('bootstrap-vue-next/components/BFormCheckbox')['BFormCheckbox']
+    BFormGroup: typeof import('bootstrap-vue-next/components/BFormGroup')['BFormGroup']
+    BFormInput: typeof import('bootstrap-vue-next/components/BFormInput')['BFormInput']
     BFormSelect: typeof import('bootstrap-vue-next/components/BFormSelect')['BFormSelect']
     BListGroup: typeof import('bootstrap-vue-next/components/BListGroup')['BListGroup']
     BListGroupItem: typeof import('bootstrap-vue-next/components/BListGroup')['BListGroupItem']
